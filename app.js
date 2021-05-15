33c32d79b9fd08ff84b1be9a3072dee0b25c1dc6
kubczakn <kubczakn@umich.edu>
2021-05-15 17:05:27 -0400

    added socket logic

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const express = require('express');
 const socketio = require('socket.io');
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// Handle index page
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Start server
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+// Initialize socket for the server
+const io = socketio(server);
+
+io.on('connection', socket => {
+    console.log("New user connected")
 });
